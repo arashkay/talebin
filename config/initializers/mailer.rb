7e2d83fc674fc55ab24b13e03e0e30eb97a7adc9
Arash <arash@tectual.com.au>
2013-08-02 19:09:12 +1000

    fix password change and mailer

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,14 @@
+ActionMailer::Base.add_delivery_method :active_record, ArMailerRails3::ActiveRecord, :email_class => Email
+ActionMailer::Base.delivery_method = :active_record
+ActionMailer::Base.smtp_settings = {
+      :address => 'mail.talebin.com',
+      :port => 587,
+      :domain => 'talebin.com',
+      :authentication => :login,
+      :user_name => 'noreply@talebin.com',
+      :password => 'zaz_419Z',
+      :enable_starttls_auto => false
+    }
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+#ActionMailer::Base.default_charset = 'utf-8'
---
+++ db/migrate/20130728121702_devise_upgrade.rb
@@ -0,0 +1,8 @@
+class DeviseUpgrade < ActiveRecord::Migration
+  def up
+    add_column :users, :reset_password_sent_at, :datetime
+  end
+
+  def down
+  end
+end
